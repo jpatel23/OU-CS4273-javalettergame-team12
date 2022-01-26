6faedcf4dc84307b96007370996764817b0f974b
johnjjacob <43150214+johnjjacob@users.noreply.github.com>
2022-01-26 17:52:08 -0600

    Added Test file

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class AppTest{
+    @Test
+    public void test1() {
+        String str1 = "test";
+        assertEquals("test", str1);
+    }
+}
+
